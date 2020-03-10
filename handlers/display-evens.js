64ffbc3c3c634ac906f795d499acbce8d586f014
Rahel <rahelsemukora@gmail.com>
2020-03-10 18:02:09 +0100

    step 2 handler done

--- handlers/display-evens.js
+++ handlers/display-evens.js
@@ -1,6 +1,19 @@
 function evensHandler() {
   debugger;
   // write me!
+  /*const nanyElement = document.getElementById('nany-button');
+  //const newEntry = document.createElement('li');
+document.getElementById('evens-button').innerHTML = strings.evens;
+*/
+strings.displayed = 'evens';
+
+const currentDisplayed = strings.displayed;
+const currentArray = strings[currentDisplayed];
+
+const currentListComponent = renderList(currentArray, currentDisplayed);
+const listContainer = document.getElementById('selected');
+  listContainer.innerHTML = '';
+  listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
